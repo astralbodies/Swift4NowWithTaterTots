8f01274d3aeedd223290e75df01d17603c901705
Aaron Douglas <astralbodies@gmail.com>
2017-08-06 11:36:59 -0500

    Adding class code

--- Swift 4 Playground.playground/Pages/Structures & Classes.xcplaygroundpage/Contents.swift
+++ Swift 4 Playground.playground/Pages/Structures & Classes.xcplaygroundpage/Contents.swift
@@ -2,6 +2,44 @@
 
 import Foundation
 
+enum TaterTotSize {
+    case small, regular, huge
+}
 
+struct TaterTotStructure {
+    let brand: String
+    let size: TaterTotSize
+    let extras: [String]
+}
+
+class TaterTotClass {
+    let brand: String
+    let size: TaterTotSize
+    let extras: [String]
+
+    init(brand: String, size: TaterTotSize, extras: [String]) {
+        self.brand = brand
+        self.size = size
+        self.extras = extras
+    }
+}
+
+let taterStruct = TaterTotStructure(brand: "Ore Ida", size: .regular, extras: [])
+let taterClass = TaterTotClass(brand: "Ore Ida", size: .regular, extras: [])
+
+struct BasicStruct {
+    var number = 0
+}
+
+class BasicClass {
+    var number = 0
+}
+
+let struct1 = BasicStruct()
+print(struct1.number)
+var struct2 = struct1 // make th
+struct2.number = 42
+print(struct1.number)
+print(struct2.number)
 
 //: [Next](@next)
